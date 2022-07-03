bef02a5e1a069911a9820e82878fed2f5e0c80c3
Amirmohammad Omumi <amirmohammad.omumi@gmail.com>
2022-07-04 01:42:17 +0430

    Add AppConfig.json For pathConnection And Class JasonRead For DeserializeObject

---
+++ MoveMyFile/JasonRead.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveMyFile
+{
+    public class JasonRead
+    {
+        public string pathConnection;
+    }
+}
