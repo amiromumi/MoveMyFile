21c4a80e52a02d72c9d60eb2ad885b1313701758
Amir Omumi <amirmohammad.omumi@gmail.com>
2025-01-03 21:57:00 +0330

    Add Change For One Years Later

--- MoveMyFile/Program.cs
+++ MoveMyFile/Program.cs
@@ -16,186 +16,200 @@ namespace MoveMyFile
         {
             Console.WriteLine("Start..." + "\n");
 
-            #region Read_JSON
-
-            string pathFileJson = File.ReadAllText(@".\AppConfig.json");
-
-            JasonRead response = JsonConvert.DeserializeObject<JasonRead>(pathFileJson);
-
-
-            #endregion
-
-            //بدست آوردن نام کاربری ویندوز
-            string userNameWindows = Environment.UserName;
-
-            string pathDesktop = "C:\\Users\\" + userNameWindows + "\\" + "Desktop";
-
-            //درصورتی که نام کاربری ونیدوز اشتباه بود از این راه بدست آور
-            if (userNameWindows == "SYSTEM")
-            {
-                pathDesktop = response.pathConnection;
-
-            }
-
             try
             {
+                string userNameWindows = Environment.UserName;
+                string pathGenerateWithUserNameWindows = "C:\\Users\\" + userNameWindows + "\\" + "Desktop";
+
+                //خواندن مسیر از فایل JSON
+                #region Read_JSON
+
+                string pathFileJson = File.ReadAllText(@".\AppConfig.json");
+                JasonRead response = JsonConvert.DeserializeObject<JasonRead>(pathFileJson);
+
+
+                //اگر داخل فایل اپ کانفیگ رشته خالی مسیر را خودت بساز و بعد از آن استفاده کن
+                if (response.pathConnection == null)
+                {
+                    response.pathConnection = new List<string>();
+                    response.pathConnection.Add(pathGenerateWithUserNameWindows);
+                }
+                #endregion
+
+
+
+                string pathDesktop = "";
+
                 while (true)
                 {
-                    string pathDesktopByFolderFiles = pathDesktop + "\\" + "Files";
-
-                    string pathFileOrganizerByTypeInRoot = @Path.Combine(Directory.GetCurrentDirectory()) + "\\File_Organizer_By_Type.bat";
-                    string pathFileOrganizerByTypeInFilesFolderDesktop = pathDesktopByFolderFiles + "\\File_Organizer_By_Type.bat";
-
-
-                    int countPathDesktop = pathDesktop.Length;
-                    List<string> filesDesktop = Directory.GetFiles(pathDesktop).ToList();
-                    List<string> filesFolder;
-                    int countPathFilesFolder = (pathDesktopByFolderFiles).Length;
-
-                    if (Directory.Exists(pathDesktopByFolderFiles))
+                    foreach (var pc in response.pathConnection)
                     {
-                        if (!File.Exists(pathFileOrganizerByTypeInFilesFolderDesktop))
+                        pathDesktop = pc;
+
+                        //اگر آرایه در فایل جیسان رشته خالی بود مسیر را خودت بساز
+                        if (pathDesktop == "")
                         {
-                            File.Copy(@pathFileOrganizerByTypeInRoot, @pathDesktopByFolderFiles + "\\File_Organizer_By_Type.bat");
+                            pathDesktop = pathGenerateWithUserNameWindows;
                         }
-                        filesFolder = Directory.GetFiles(pathDesktopByFolderFiles).ToList();
-                        MoveFiles();
-                    }
-                    else
-                    {
-                        Directory.CreateDirectory(pathDesktopByFolderFiles);
-                        File.Copy(@pathFileOrganizerByTypeInRoot, @pathDesktopByFolderFiles + "\\File_Organizer_By_Type.bat");
-                        filesFolder = Directory.GetFiles(pathDesktopByFolderFiles).ToList();
-                        MoveFiles();
-                    }
 
-                    void MoveFiles()
-                    {
-                        List<string> filesDesktopOld = new List<string>();
+                        string pathDesktopByFolderFiles = pathDesktop + "\\" + "Files";
 
-                        //لیست فایل های جدید هم نام در دسکتاپ
-                        List<string> dsNewList = new List<string>();
+                        string pathFileOrganizerByTypeInRoot = @Path.Combine(Directory.GetCurrentDirectory()) + "\\File_Organizer_By_Type.bat";
+                        string pathFileOrganizerByTypeInFilesFolderDesktop = pathDesktopByFolderFiles + "\\File_Organizer_By_Type.bat";
 
-                        foreach (var ds in filesDesktop)
+
+                        int countPathDesktop = pathDesktop.Length;
+                        List<string> filesDesktop = Directory.GetFiles(pathDesktop).ToList();
+                        List<string> filesFolder;
+                        int countPathFilesFolder = (pathDesktopByFolderFiles).Length;
+
+                        if (Directory.Exists(pathDesktopByFolderFiles))
                         {
-                            filesDesktopOld.Add(ds);
-                            dsNewList.Add(ds);
-                            foreach (var fs in filesFolder)
+                            if (!File.Exists(pathFileOrganizerByTypeInFilesFolderDesktop))
                             {
-                                string subDs = ds.Substring(countPathDesktop + 1, (ds.Length - (countPathDesktop + 1)));
-                                string subFs = fs.Substring(countPathFilesFolder + 1, (fs.Length - (countPathFilesFolder + 1)));
-                                string dsNew = ds.Substring(0, countPathDesktop);
+                                File.Copy(@pathFileOrganizerByTypeInRoot, @pathDesktopByFolderFiles + "\\File_Organizer_By_Type.bat");
+                            }
+                            filesFolder = Directory.GetFiles(pathDesktopByFolderFiles).ToList();
+                            MoveFiles();
+                        }
+                        else
+                        {
+                            Directory.CreateDirectory(pathDesktopByFolderFiles);
+                            File.Copy(@pathFileOrganizerByTypeInRoot, @pathDesktopByFolderFiles + "\\File_Organizer_By_Type.bat");
+                            filesFolder = Directory.GetFiles(pathDesktopByFolderFiles).ToList();
+                            MoveFiles();
+                        }
 
+                        void MoveFiles()
+                        {
+                            List<string> filesDesktopOld = new List<string>();
 
-                                var filesSameInDesktop = filesFolder.Where(f => f.Contains(subDs)).ToList();
-                                string maxFilesSameInDesktop = "";
-                                if (filesSameInDesktop.Count > 1)
+                            //لیست فایل های جدید هم نام در دسکتاپ
+                            List<string> dsNewList = new List<string>();
+
+                            foreach (var ds in filesDesktop)
+                            {
+                                filesDesktopOld.Add(ds);
+                                dsNewList.Add(ds);
+                                foreach (var fs in filesFolder)
                                 {
-                                    //پیدا کردن بزرگ ترین رشته هم نام
-                                    for (int i = 0; i < filesSameInDesktop.Count; i++)
+                                    string subDs = ds.Substring(countPathDesktop + 1, (ds.Length - (countPathDesktop + 1)));
+                                    string subFs = fs.Substring(countPathFilesFolder + 1, (fs.Length - (countPathFilesFolder + 1)));
+                                    string dsNew = ds.Substring(0, countPathDesktop);
+
+
+                                    var filesSameInDesktop = filesFolder.Where(f => f.Contains(subDs)).ToList();
+                                    string maxFilesSameInDesktop = "";
+                                    if (filesSameInDesktop.Count > 1)
                                     {
-                                        maxFilesSameInDesktop = filesSameInDesktop[0];
-                                        if (filesSameInDesktop[i].Length > maxFilesSameInDesktop.Length)
+                                        //پیدا کردن بزرگ ترین رشته هم نام
+                                        for (int i = 0; i < filesSameInDesktop.Count; i++)
                                         {
-                                            maxFilesSameInDesktop = filesSameInDesktop[i];
+                                            maxFilesSameInDesktop = filesSameInDesktop[0];
+                                            if (filesSameInDesktop[i].Length > maxFilesSameInDesktop.Length)
+                                            {
+                                                maxFilesSameInDesktop = filesSameInDesktop[i];
+                                            }
+                                        }
+                                        if (maxFilesSameInDesktop != "")
+                                        {
+                                            maxFilesSameInDesktop = maxFilesSameInDesktop.
+                                            Substring(countPathFilesFolder + 1, (maxFilesSameInDesktop.Length - (countPathFilesFolder + 1)));
+                                        }
+
+                                        if (filesSameInDesktop.Count >= 1)
+                                        {
+                                            dsNew = dsNew + "\\" + "V_" + maxFilesSameInDesktop;
+                                            if (!File.Exists(dsNew))
+                                            {
+                                                File.Move(ds, dsNew);
+                                                dsNewList.Remove(ds);
+                                                dsNewList.Add(dsNew);
+                                            }
+                                            dsNew = ds.Substring(0, countPathDesktop);
+                                        }
+
+                                        if (filesSameInDesktop.Count >= 1)
+                                        {
+                                            var filesDesktopNew = Directory.GetFiles(pathDesktop).ToList();
+                                            foreach (var fde in filesDesktopNew)
+                                            {
+                                                string subFde = fde.Substring(countPathDesktop + 1, (fde.Length - (countPathDesktop + 1)));
+                                                if (subFde == subDs)
+                                                {
+                                                    dsNew = dsNew + "\\" + "V_" + subFde;
+
+                                                    File.Move(ds, dsNew);
+                                                    dsNewList.Add(dsNew);
+                                                }
+                                            }
                                         }
                                     }
-                                    if (maxFilesSameInDesktop != "")
+                                    else
                                     {
-                                        maxFilesSameInDesktop = maxFilesSameInDesktop.
-                                        Substring(countPathFilesFolder + 1, (maxFilesSameInDesktop.Length - (countPathFilesFolder + 1)));
-                                    }
 
-                                    if (filesSameInDesktop.Count >= 1)
-                                    {
-                                        dsNew = dsNew + "\\" + "V_" + maxFilesSameInDesktop;
-                                        if (!File.Exists(dsNew))
+                                        if (subDs == subFs)
                                         {
+                                            dsNew = dsNew + "\\" + "V_" + subDs;
                                             File.Move(ds, dsNew);
                                             dsNewList.Remove(ds);
                                             dsNewList.Add(dsNew);
                                         }
-                                        dsNew = ds.Substring(0, countPathDesktop);
-                                    }
-
-                                    if (filesSameInDesktop.Count >= 1)
-                                    {
-                                        var filesDesktopNew = Directory.GetFiles(pathDesktop).ToList();
-                                        foreach (var fde in filesDesktopNew)
+                                        else
                                         {
-                                            string subFde = fde.Substring(countPathDesktop + 1, (fde.Length - (countPathDesktop + 1)));
-                                            if (subFde == subDs)
+                                            if (dsNewList.Any(a => a == subDs))
                                             {
-                                                dsNew = dsNew + "\\" + "V_" + subFde;
-
-                                                File.Move(ds, dsNew);
+                                                dsNew = dsNew + subDs;
                                                 dsNewList.Add(dsNew);
                                             }
                                         }
                                     }
-                                }
-                                else
-                                {
 
-                                    if (subDs == subFs)
-                                    {
-                                        dsNew = dsNew + "\\" + "V_" + subDs;
-                                        File.Move(ds, dsNew);
-                                        dsNewList.Remove(ds);
-                                        dsNewList.Add(dsNew);
-                                    }
-                                    else
-                                    {
-                                        if (dsNewList.Any(a => a == subDs))
-                                        {
-                                            dsNew = dsNew + subDs;
-                                            dsNewList.Add(dsNew);
-                                        }
-                                    }
                                 }
-
                             }
-                        }
 
-                        foreach (var list in dsNewList)
-                        {
-                            filesDesktop.Add(list);
-                        }
-
-                        if (dsNewList.Count != 0)
-                        {
-                            foreach (var list in filesDesktopOld)
+                            foreach (var list in dsNewList)
                             {
-                                filesDesktop.Remove(list);
+                                filesDesktop.Add(list);
+                            }
+
+                            if (dsNewList.Count != 0)
+                            {
+                                foreach (var list in filesDesktopOld)
+                                {
+                                    filesDesktop.Remove(list);
+                                }
+                            }
+
+
+                            foreach (var item in filesDesktop)
+                            {
+                                File.Move(item,
+                                    $"{pathDesktop}\\Files\\{item.Substring(countPathDesktop + 1, (item.Length - (countPathDesktop + 1)))}"
+                                    );
+
+                                Console.WriteLine(item);
                             }
                         }
 
-
-                        foreach (var item in filesDesktop)
+                        //متد مرتب سازی فایل در پوشه ها بر اساس نوع فایل
+                        void FileOrganizerByType()
                         {
-                            File.Move(item,
-                                $"{pathDesktop}\\Files\\{item.Substring(countPathDesktop + 1, (item.Length - (countPathDesktop + 1)))}"
-                                );
-
-                            Console.WriteLine(item);
+                            Process proc = null;
+                            string batDir = pathDesktopByFolderFiles;
+                            proc = new Process();
+                            proc.StartInfo.WorkingDirectory = batDir;
+                            proc.StartInfo.FileName = "File_Organizer_By_Type.bat";
+                            proc.StartInfo.CreateNoWindow = false;
+                            proc.Start();
+                            proc.WaitForExit();
+                            Console.WriteLine($"... Bat file executed For ({pathDesktop}) !! ...");
                         }
-                    }
 
-                    void FileOrganizerByType()
-                    {
-                        Process proc = null;
-                        string batDir = pathDesktopByFolderFiles;
-                        proc = new Process();
-                        proc.StartInfo.WorkingDirectory = batDir;
-                        proc.StartInfo.FileName = "File_Organizer_By_Type.bat";
-                        proc.StartInfo.CreateNoWindow = false;
-                        proc.Start();
-                        proc.WaitForExit();
-                        Console.WriteLine("Bat file executed !!");
+                        FileOrganizerByType();
+                        Console.WriteLine($"......... End Move Files {pathDesktop} ........." + "\n");
                     }
-
                     Thread.Sleep(7200000);
-                    FileOrganizerByType();
                 }
 
                 Console.ReadKey();
